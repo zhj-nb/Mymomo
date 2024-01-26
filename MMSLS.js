04d6b6cdaf294a55d8ce3901795e3ebc015e4005
zhj-nb <70524535+zhj-nb@users.noreply.github.com>
2024-01-27 00:20:53 +0800

    更新 MMSLS.js

--- MMSLS.js
+++ MMSLS.js
@@ -19,7 +19,7 @@ const MMSLS = [
 =====================请在下面编辑=====================
  */
 const MMSLS = [
-    "https://www.maimemo.com/share/page?uid=9357519&pid=14bf2950f93c05efc461fbea94fcc973&tid=9d2279556ba7018a012f5c92e8b9509d",
+    "https://www.maimemo.com/share/page?uid=9357519&pid=095a70b425b3b1cbab248241825aab38&tid=586541ae2eb54a7d377f8bd2b384da31",
   ]
   /**
    * 生成随机数字
